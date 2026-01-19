07d12c0ff2d4fe39cebc5d58ee80e46b5cc62326
tovagiventer <tovagiventer@gmail.com>
2026-01-19 21:35:36 +0200

    w5d2 XP+ corrections

--- DI170/Week5/Day2/Day2Submissions/ExerciseXP/ExerciseXP+.sql
+++ DI170/Week5/Day2/Day2Submissions/ExerciseXP/ExerciseXP+.sql
@@ -1,9 +1,11 @@
--- CREATE TABLE student(
---  student_id SERIAL PRIMARY KEY,
---  first_name VARCHAR (50) NOT NULL,
---  last_name VARCHAR (100) NOT NULL,
---  birth_date DATE NOT NULL
--- )
+CREATE DATABASE bootcamp;
+
+CREATE TABLE students(
+ id SERIAL PRIMARY KEY,
+ first_name VARCHAR (50) NOT NULL,
+ last_name VARCHAR (100) NOT NULL,
+ birth_date DATE NOT NULL
+)
 
 -- Create a database called bootcamp.
 -- Create a table called students.
@@ -21,18 +23,18 @@
 -- 5	David	Grez	14/06/2003
 -- 6	Omer	Simpson	03/10/1980
 
--- INSERT INTO student (first_name, last_name, birth_date)
--- VALUES
---   ('Marc',	'Benichou',	'1998-11-02'),
---   ('Yoan',	'Cohen',	'2010-12-03'),
---   ('Lea',	'Benichou',	'1987-07-27'),
---   ('Amelia',	'Dux',	'1996-04-07'),
---   ('David',	'Grez',	'2003-06-14'),
---   ('Omer',	'Simpson',	'1980-10-03');
+INSERT INTO student (first_name, last_name, birth_date)
+VALUES
+  ('Marc',	'Benichou',	'1998-11-02'),
+  ('Yoan',	'Cohen',	'2010-12-03'),
+  ('Lea',	'Benichou',	'1987-07-27'),
+  ('Amelia',	'Dux',	'1996-04-07'),
+  ('David',	'Grez',	'2003-06-14'),
+  ('Omer',	'Simpson',	'1980-10-03');
 
 -- Insert your last_name, first_name and birth_date to the students table (Take a look at the id given).
--- INSERT INTO student (last_name, first_name, birth_date)
--- VALUES ('Giventer', 'Tova', '10/28/1986');
+INSERT INTO student (last_name, first_name, birth_date)
+VALUES ('Giventer', 'Tova', '1986-10-28');
 
 -- Fetch all of the data from the table.
 SELECT * from student;
