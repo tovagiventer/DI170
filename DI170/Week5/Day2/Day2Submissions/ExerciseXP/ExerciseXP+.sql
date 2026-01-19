a36f1c0499aba7f6c6fbf19307d68b36bac477a1
tovagiventer <tovagiventer@gmail.com>
2026-01-19 22:02:59 +0200

    XP+ fixes 2nd try

--- DI170/Week5/Day2/Day2Submissions/ExerciseXP/ExerciseXP+.sql
+++ DI170/Week5/Day2/Day2Submissions/ExerciseXP/ExerciseXP+.sql
@@ -23,7 +23,7 @@ CREATE TABLE students(
 -- 5	David	Grez	14/06/2003
 -- 6	Omer	Simpson	03/10/1980
 
-INSERT INTO student (first_name, last_name, birth_date)
+INSERT INTO students (first_name, last_name, birth_date)
 VALUES
   ('Marc',	'Benichou',	'1998-11-02'),
   ('Yoan',	'Cohen',	'2010-12-03'),
@@ -33,30 +33,30 @@ VALUES
   ('Omer',	'Simpson',	'1980-10-03');
 
 -- Insert your last_name, first_name and birth_date to the students table (Take a look at the id given).
-INSERT INTO student (last_name, first_name, birth_date)
+INSERT INTO students (last_name, first_name, birth_date)
 VALUES ('Giventer', 'Tova', '1986-10-28');
 
 -- Fetch all of the data from the table.
-SELECT * from student;
+SELECT * from students;
 -- Fetch all of the students first_names and last_names.
-SELECT first_name, last_name FROM student;
+SELECT first_name, last_name FROM students;
 -- For the following questions, only fetch the first_names and last_names of the students.
 -- -- Fetch the student which id is equal to 2.
-SELECT first_name, last_name FROM student WHERE student_id=2;
+SELECT first_name, last_name FROM students WHERE id=2;
 -- Fetch the student whose last_name is Benichou AND first_name is Marc.
-SELECT first_name, last_name FROM student WHERE last_name = 'Benichou' AND first_name = 'Marc';
+SELECT first_name, last_name FROM students WHERE last_name = 'Benichou' AND first_name = 'Marc';
 -- Fetch the students whose last_names are Benichou OR first_names are Marc.
-SELECT first_name, last_name FROM student WHERE last_name = 'Benichou' OR first_name = 'Marc';
+SELECT first_name, last_name FROM students WHERE last_name = 'Benichou' OR first_name = 'Marc';
 -- Fetch the students whose first_names contain the letter a.
-SELECT first_name, last_name FROM student WHERE first_name LIKE '%a%';
+SELECT first_name, last_name FROM students WHERE first_name LIKE '%a%';
 -- Fetch the students whose first_names start with the letter a.
-SELECT first_name, last_name FROM student WHERE first_name LIKE 'a%';
+SELECT first_name, last_name FROM students WHERE first_name LIKE 'a%';
 -- Fetch the students whose first_names end with the letter a.
-SELECT first_name, last_name FROM student WHERE first_name LIKE '%a';
+SELECT first_name, last_name FROM students WHERE first_name LIKE '%a';
 -- Fetch the students whose second to last letter of their first_names are a (Example: Leah).
-SELECT first_name, last_name FROM student WHERE first_name LIKE '%a_';
+SELECT first_name, last_name FROM students WHERE first_name LIKE '%a_';
 -- Fetch the students whose id’s are equal to 1 AND 3 .
-SELECT first_name, last_name FROM student WHERE student_id=1 OR student_id=3;
+SELECT first_name, last_name FROM students WHERE id=1 OR id=3;
 
 -- Fetch the students whose birth_dates are equal to or come after 1/01/2000. (show all their info).
-SELECT * FROM student WHERE birth_date >= '1/01/2000';
+SELECT * FROM students WHERE birth_date >= '2000-01-01';
